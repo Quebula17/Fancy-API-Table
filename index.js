510e717a2a5251d710447ac3e0fcd7435df516b1
Saurabh Rana <Saurabh_r@bt.iitr.ac.in>
2023-05-19 05:12:53 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+fetch('https://reqres.in/api/users')
+    .then(response => response.json())
+    .then(data => {
+        const users = data.data;
+
+        const tableBody = document.getElementById('table-body')
+
+        users.forEach(user => {
+            const tableRow = document.createElement('tr');
+
+            const serialNumber = document.createElement('td');
+            serialNumber.innerHTML = user.id;
+
+            const email = document.createElement('td');
+            email.innerHTML = user.email;
+
+            const firstName = document.createElement('td');
+            firstName.innerHTML = user.first_name;
+
+            const lastName = document.createElement('td');
+            lastName.innerHTML = user.last_name;
+
+
+            const avatar = document.createElement('td');
+            const image = document.createElement('img');
+
+            image.src = user.avatar;
+            avatar.appendChild(image);
+
+            
+            tableRow.appendChild(serialNumber);
+            tableRow.appendChild(email);
+            tableRow.appendChild(firstName);
+            tableRow.appendChild(lastName);
+            tableRow.appendChild(avatar);
+
+            tableBody.appendChild(tableRow);
+        })
+    })
